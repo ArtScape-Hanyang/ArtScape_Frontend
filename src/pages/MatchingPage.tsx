6799e14a7d477206601ba46b3fb50f7eea62e20f
Ain1204 <emily030528@gmail.com>
2024-11-12 01:24:20 +0900

    feat/grid

--- src/pages/MatchingPage.tsx
+++ src/pages/MatchingPage.tsx
@@ -7,8 +7,11 @@ const MatchingPage = () => {
     <div className="main-container">
       <div className="container">
         <div className="container--icon">
-          <img src={backbtn} className="back-icon" alt="뒤로가기" />
+          <button className="back-button">
+            <img src={backbtn} className="back-icon" alt="뒤로가기" />
+          </button>
         </div>
+
         <div className="text-container">
           <div className="text-container--title">
             <h2>AI가 찾아주는</h2>
@@ -19,14 +22,13 @@ const MatchingPage = () => {
           <img src={aimatchstar} className="back-star" alt="배경이미지,," />
         </div>
         <div className="start--container">
-          {" "}
           <div className="text-container">
-            <div className="text-container--next">
-              <h5>시작하기버튼!</h5>
-            </div>
-            <div className="container--icon">
-              <img src={nextbtn} className="next-icon" alt="다음으로" />
-            </div>
+            <button className="start-button">
+              <h5>시작하기</h5>
+              <div className="container--icon">
+                <img src={nextbtn} className="next-icon" alt="다음으로" />
+              </div>
+            </button>
           </div>
         </div>
         {/* 페이지 내용 */}
