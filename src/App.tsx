2b231fde483401fcf84ca590e0ec32bfb51b9f69
yellowmodric <87848941+yellowmodric@users.noreply.github.com>
2024-11-16 02:11:26 +0900

    [design] : 단체전 매칭 인원 수 페이지 완료 / 상호작용 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/home";
-import MatchingPage from "./pages/MatchingPage";
 import MatchTypePage from "./pages/MatchTypePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import MatchPersonnel from "./pages/MatchPersonnel";
 
 const App: React.FC = () => {
   return (
@@ -13,8 +13,8 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/matching" element={<MatchingPage />} />
           <Route path="/matching/type" element={<MatchTypePage />}></Route>
+          <Route path="/matching/personnel" element={<MatchPersonnel />}></Route>
         </Routes>
       </div>
     </Router>
