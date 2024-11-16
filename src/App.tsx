a2ea8f5a8ed8ec085ab80831e58b18b86fb5dd2b
Ain1204 <emily030528@gmail.com>
2024-11-17 00:19:20 +0900

    feat/loginpage수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/home";
 
-import MatchingPage from "./pages/MatchingPage";
 import MatchTypePage from "./pages/MatchTypePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
@@ -10,15 +9,12 @@ import SignupPage from "./pages/SignupPage";
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/matching" element={<MatchingPage />} />
-          <Route path="/matching/type" element={<MatchTypePage />}></Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/matching/type" element={<MatchTypePage />}></Route>
+      </Routes>
     </Router>
   );
 };
